9de99dce5b563ae4a427e8c6d3afc3d76293d7e7
PEPAKAYALA TEJA <pepakayalateja06082003@gmail.com>
2026-08-05 17:49:27 +0530

    ython_intro

---
+++ Day-02/keywords.py
@@ -0,0 +1,5 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=================================================================== RESTART: D:/TEJA/Documents/keywords.py ==================================================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
---
+++ Day-02/keywords1.py
@@ -0,0 +1,24 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+==================== RESTART: D:/TEJA/Documents/keywords.py ====================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+>>> a=20
+>>> b=30
+>>> a
+20
+>>> b
+30
+>>> a=b=20-10
+>>> a
+10
+>>> del a
+>>> a
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    a
+NameError: name 'a' is not defined
+>>> a=12
+>>> a,b=b,a
+>>> a-b
+-2
